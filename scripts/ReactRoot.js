c898480834690659762b31a7b0d60ab92786445a
Akil Harris <akil.harris@vice.com>
2016-03-28 11:55:22 -0400

    changes header

--- scripts/ReactRoot.js
+++ scripts/ReactRoot.js
@@ -7,7 +7,7 @@ export default class Root extends Component {
   render() {
     return (
       <div>
-      	<h1>Drone Strikes</h1>
+      	<h1>Covert US Drone Strikes</h1>
         <Map />
       </div>
     );
